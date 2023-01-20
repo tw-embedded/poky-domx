2b085f81d03d7f066dc243b5e38ca0776e28bf81
alix.gao <alix.gao@qq.com>
2023-01-20 17:13:32 +0800

    fake dom0
    Signed-off-by: alix.gao <alix.gao@qq.com>

---
+++ build.sh
@@ -0,0 +1,17 @@
+
+source ./oe-init-build-env
+
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-filesystems
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+bitbake-layers add-layer ../meta-virtualization
+
+echo "" >> conf/local.conf
+echo "MACHINE = \"fake-arm64\"" >> conf/local.conf
+echo "DISTRO = \"poky\"" >> conf/local.conf
+echo "IMAGE_FSTYPES += \"cpio.gz\"" >> conf/local.conf
+echo "DISTRO_FEATURES += \" virtualization xen\"" >> conf/local.conf
+echo "BUILD_REPRODUCIBLE_BINARIES = \"1\"" >> conf/local.conf
+echo "" >> conf/local.conf
+
