64b07edcc98b61c86627d22e9fcbd052edb72f0a
alix.gao <alix.gao@qq.com>
2023-02-01 21:03:17 +0800

    add systemd feature
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo "" >> conf/local.conf
 echo "MACHINE = \"fake-arm64\"" >> conf/local.conf
 echo "DISTRO = \"poky\"" >> conf/local.conf
 echo "IMAGE_FSTYPES += \"cpio.gz\"" >> conf/local.conf
-echo "DISTRO_FEATURES += \" virtualization xen\"" >> conf/local.conf
+echo "DISTRO_FEATURES += \" virtualization xen systemd\"" >> conf/local.conf
 echo "BUILD_REPRODUCIBLE_BINARIES = \"1\"" >> conf/local.conf
 echo "" >> conf/local.conf
 
