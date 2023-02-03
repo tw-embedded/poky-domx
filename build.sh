0c3dd90ada028116683f554c10bf5bc5896e8855
alix.gao <alix.gao@qq.com>
2023-02-03 22:04:40 +0800

    try to rm qemu
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- build.sh
+++ build.sh
@@ -11,11 +11,12 @@ echo "" >> conf/local.conf
 echo "MACHINE = \"fake-arm64\"" >> conf/local.conf
 echo "DISTRO = \"poky\"" >> conf/local.conf
 echo "IMAGE_FSTYPES += \"cpio.gz\"" >> conf/local.conf
-echo "DISTRO_FEATURES += \" xen systemd\"" >> conf/local.conf
+echo "DISTRO_FEATURES += \" virtualization xen systemd\"" >> conf/local.conf
 echo "DISTRO_FEATURES_BACKFILL_CONSIDERED += \"sysvinit\"" >> conf/local.conf
 echo "VIRTUAL-RUNTIME_init_manager = \"systemd\"" >> conf/local.conf
 echo "VIRTUAL-RUNTIME_initscripts = \"systemd-compat-units\"" >> conf/local.conf
 echo "BUILD_REPRODUCIBLE_BINARIES = \"1\"" >> conf/local.conf
+echo "EXTRA_IMAGEDEPENDS_remove += \" qemu-native\""
 echo "" >> conf/local.conf
 
 bitbake fake-dom0
