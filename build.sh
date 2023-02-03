b2a5f5f648e4b426ecc2710c628c8c31d20e427a
alix.gao <alix.gao@qq.com>
2023-02-03 23:52:29 +0800

    try rm qemu
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ echo "VIRTUAL-RUNTIME_init_manager = \"systemd\"" >> conf/local.conf
 echo "VIRTUAL-RUNTIME_initscripts = \"systemd-compat-units\"" >> conf/local.conf
 echo "BUILD_REPRODUCIBLE_BINARIES = \"1\"" >> conf/local.conf
 echo "EXTRA_IMAGEDEPENDS_remove += \" qemu-native\""
+echo "IMAGE_INSTALL_remove += \" qemu-native\""
 echo "" >> conf/local.conf
 
 bitbake fake-dom0
